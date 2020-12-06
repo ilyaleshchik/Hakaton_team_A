3e12dcf644d30ecd68f1159debeed441ed07b39b
ilyaleshchik <lurkudismal88@gmail.com>
2020-12-06 23:22:38 +0300

    Добавил пункты

---
+++ Пункт 4/Divide/Divide.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+FILES = []
+
+
+def GetFiles():
+    global FILES
+    FILES = [f for f in os.listdir("Inputs") if os.path.isfile(os.path.join("Inputs", f))]
+
+
+def main():
+    GetFiles()
+    global FILES
+    for FILE in FILES:
+        os.chdir("Inputs")
+        fin = open(FILE, "r")
+        os.chdir("../")
+        os.chdir("Outputs")
+        fout = open(FILE, "w")
+        os.chdir("../")
+        tmp = ""
+        for line in fin.readlines():
+            a = line.split(' ')
+            b = [x for x in a if x != '']
+            tmp += b[1]
+            if(b[2] == 'S'):
+                if(len(tmp) > 9):
+                    fout.write(tmp + "\n")
+                tmp = ""
+        if(len(tmp) > 9):
+            fout.write(tmp + "\n")
+        tmp = ""
+
+
+
+if __name__ == '__main__':
+    main()
